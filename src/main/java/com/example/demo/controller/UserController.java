9372da5008ac000bd4ed1f4a83025c528ced4d2b
nguyenthedat <dnguyenthe706@gmail.com>
2026-02-12 21:57:04 +0700

    add crossOrigin

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -24,7 +24,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @Validated
 
